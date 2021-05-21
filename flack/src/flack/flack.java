ae2d9186e00f477ed1418ad516ca04f2113cbfbf
Annan <mbebwoannan@gmail.com>
2021-05-21 10:22:13 -0400

    meo

---
+++ flack/src/flack/flack.java
@@ -0,0 +1,15 @@
+package flack;
+
+public class flack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("annan");
+		int annan = 0;
+		int flash = 30;
+		System.out.println("annan");
+		
+
+	}
+
+}
